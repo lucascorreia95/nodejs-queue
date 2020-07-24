668fda0c856cedc89ab655503c4ee84aa4deb65a
Lucas Correia <lucascorreiaemail@gmail.com>
2020-07-24 17:26:46 -0300

    style: eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (_req, res) => {
-  res.send({ message: "Hello World!" });
+app.get('/', (_req, res) => {
+  res.send({ message: 'Hello World!' });
 });
 
-app.listen(3333, () => console.log("Server is listening on port 3333"));
+app.listen(3333, () => console.log('Server is listening on port 3333'));
