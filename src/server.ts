6d25cbb3d16cdf689798785c2f7023091c82d192
Lucas Correia <lucascorreiaemail@gmail.com>
2020-07-24 15:53:30 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+app.listen(3333, () => console.log("Server is listening on port 3333"));
