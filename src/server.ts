d2affdf5b321b88bef21bfc2737b655591ccf89b
Lucas Correia <lucascorreiaemail@gmail.com>
2020-07-25 17:41:44 -0300

    feat: queues dashboard

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
+import BullBoard from 'bull-board';
 import UserController from './app/controllers/UserController';
+import Queue from './app/lib/Queue';
 
 const app = express();
+BullBoard.setQueues(Queue.queues.map((queue) => queue.bull));
 
 app.use(express.json());
 
 app.post('/users', UserController.store);
+
+app.use('/admin/queues', BullBoard.UI);
 
 app.listen(3333, () => console.log('Server is listening on port 3333'));
